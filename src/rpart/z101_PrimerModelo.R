46400c6b34d8b95ef9e8913e990a83e348463f4c
abbutinn <aguizquierdo@gmail.com>
2024-03-26 18:52:46 -0300

    primer push mi repo

--- src/rpart/z101_PrimerModelo.R
+++ src/rpart/z101_PrimerModelo.R
@@ -1,13 +1,16 @@
 # Arbol elemental con libreria  rpart
 # Debe tener instaladas las librerias  data.table  ,  rpart  y  rpart.plot
-
+rm(list = ls())
 # cargo las librerias que necesito
 require("data.table")
+library("data.table")
 require("rpart")
+library("rpart")
 require("rpart.plot")
+library("rpart.plot")
 
 # Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("X:\\gdrive\\labo2024v1\\") # Establezco el Working Directory
+setwd("/Users/Agu/Documents/git/labo1/") # Establezco el Working Directory
 
 # cargo el dataset
 dataset <- fread("./datasets/dataset_pequeno.csv")
@@ -17,7 +20,8 @@ dapply <- dataset[foto_mes == 202109] # defino donde voy a aplicar el modelo
 
 # genero el modelo,  aqui se construye el arbol
 # quiero predecir clase_ternaria a partir de el resto de las variables
-modelo <- rpart(
+#inicial cp =-0.3, minsplit = 0, minbucket =1, maxdepth =3 
+modelo_base <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
@@ -27,31 +31,60 @@ modelo <- rpart(
         maxdepth = 3
 ) # profundidad maxima del arbol
 
+##con cp =-0.3, minsplit = 166, minbucket =324, maxdepth =6 -> 11117 
+modelo1 <- rpart(
+  formula = "clase_ternaria ~ .",
+  data = dtrain, # los datos donde voy a entrenar
+  xval = 0,
+  cp = -0.9, # esto significa no limitar la complejidad de los splits
+  minsplit = 166, # minima cantidad de registros para que se haga el split
+  minbucket = 324, # tamaño minimo de una hoja
+  maxdepth = 6
+) # profundidad maxima del arbol
+
 
 # grafico el arbol
-prp(modelo,
+prp(modelo1,
         extra = 101, digits = -5,
         branch = 1, type = 4, varlen = 0, faclen = 0
 )
 
 
 # aplico el modelo a los datos nuevos
-prediccion <- predict(
-        object = modelo,
+prediccion_base <- predict(
+        object = modelo_base,
         newdata = dapply,
         type = "prob"
 )
-
+#############agu
+# aplico el modelo a los datos nuevos
+prediccion_1 <- predict(
+  object = modelo1,
+  newdata = dapply,
+  type = "prob"
+)
+#####agu 
 # prediccion es una matriz con TRES columnas,
 # llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
 # cada columna es el vector de probabilidades
 
 # agrego a dapply una columna nueva que es la probabilidad de BAJA+2
-dapply[, prob_baja2 := prediccion[, "BAJA+2"]]
+dapply[, prob_baja2 := prediccion_base[, "BAJA+2"]]
 
 # solo le envio estimulo a los registros
 #  con probabilidad de BAJA+2 mayor  a  1/40
-dapply[, Predicted := as.numeric(prob_baja2 > 1 / 40)]
+dapply[, Predicted_base := as.numeric(prob_baja2 > 1 / 40)]
+
+#####modelo1
+# agrego a dapply una columna nueva que es la probabilidad de BAJA+2
+dapply[, prob_baja2_mod1 := prediccion_1[, "BAJA+2"]]
+
+# solo le envio estimulo a los registros
+#  con probabilidad de BAJA+2 mayor  a  1/40
+dapply[, Predicted_1 := as.numeric(prob_baja2_mod1 > 1 / 40)]
+
+#####
+
 
 # genero el archivo para Kaggle
 # primero creo la carpeta donde va el experimento
@@ -59,7 +92,8 @@ dir.create("./exp/")
 dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
-fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_001.csv",
+fwrite(dapply[, list(numero_de_cliente, Predicted_1)],
+        file = "./exp/KA2001/K101_001_3.csv",
         sep = ","
 )
+
